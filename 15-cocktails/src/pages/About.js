c2814412581a1d6f878376bae0a4627b4d0de6fb
Rurutia1027 <rurutia1027@gmail.com>
2025-01-22 18:40:42 +0800

    update project 15 in react js

---
+++ 15-cocktails/src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+        dolorum animi consequatur aspernatur asperiores voluptate, ut delectus
+        obcaecati esse deleniti maiores aliquid minus molestias ea consequuntur
+        quos ad provident nulla! Lorem ipsum, dolor sit amet consectetur
+        adipisicing elit. Ab, illum, quis sequi exercitationem in veniam atque
+        itaque aut natus rem facere a consequatur quaerat repellendus magni
+        mollitia est optio ipsa!
+      </p>
+    </section>
+  )
+}
+
+export default About
