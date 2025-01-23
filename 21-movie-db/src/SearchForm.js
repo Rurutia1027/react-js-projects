c005640ff38a5cadcb38ee8e793d1267658f629a
Rurutia1027 <rurutia1027@gmail.com>
2025-01-23 16:40:01 +0800

    update codes for project 21

---
+++ 21-movie-db/src/SearchForm.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useGlobalContext } from './context'
+const SearchForm = () => {
+  const { query, setQuery, error } = useGlobalContext()
+  return (
+    <form className='search-form'>
+      <h2>search movies</h2>
+      <input
+        type='text'
+        className='form-input'
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </form>
+  )
+}
+
+export default SearchForm
